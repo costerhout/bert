201069b7a40e11a8cf61cfb0d807e3497e490c67
Colin Osterhout <colixx@gmail.com>
2016-04-11 12:26:11 -0800

    First working version

--- src/js/widgets/map.js
+++ src/js/widgets/map.js
@@ -6,19 +6,23 @@
         // the necessary attributes
         $('div.uas-widget-map[data-map-src][data-map-type]').each(function () {
             var el = this,
-                map = {},
+                map = undefined,    // Set to undefined to check for init later on
                 locations = [],
+                locationsShow = [],
                 mapTypeId,
+                centerPos = {},
                 showId = '';
 
             // Determine map type
-            switch ($(this).attr('data-map-type')) {
-                case 'hybrid': mapTypeId = google.maps.MapTypeId.HYBRID; break;
-                case 'roadmap': mapTypeId = google.maps.MapTypeId.ROADMAP; break;
-                case 'satellite': mapTypeId = google.maps.MapTypeId.SATELLITE; break;
-                case 'terrain': mapTypeId = google.maps.MapTypeId.TERRAIN; break;
-                default: mapTypeId = google.maps.MapTypeId.HYBRID;
-            }
+            mapTypeId = {
+                hybrid: google.maps.MapTypeId.HYBRID,
+                roadmap: google.maps.MapTypeId.ROADMAP,
+                satellite: google.maps.MapTypeId.SATELLITE,
+                terrain: google.maps.MapTypeId.TERRAIN
+            }[$(this).attr('data-map-type')];
+
+            // Default case
+            mapTypeId = typeof mapTypeId === 'undefined' ? google.maps.MapTypeId.HYBRID : mapTypeId;
 
             // See if we're supposed to show a particular point
             if ($(this).attr('data-map-show') !== undefined) {
@@ -31,29 +35,28 @@
                 dataType: 'xml'
             }).done(function (xml) {
                 // Parse XML response - get center point location and zoom level
-                var locationsShow, centerPos,
-                    centerLat = Number($('system-data-structure > latitude', xml).first().html()),
-                    centerLng = Number($('system-data-structure > longitude', xml).first().html()),
+                centerPos = new google.maps.LatLng(
+                        Number($('system-data-structure > latitude', xml).first().html()),
+                        Number($('system-data-structure > longitude', xml).first().html())
+                    ),
                     zoom = Number($('system-data-structure > zoom', xml).first().html());
 
                 // Create a new location in an array and populate
                 // based on the values in the XML response
                 $('point', xml).each(function () {
-                    var id, lat, lng, title, content, show;
-                    id = $('id', this).first().html();
-                    lat = Number($('latitude', this).first().html());
-                    lng = Number($('longitude', this).first().html());
-                    title = $('label', this).first().html();
-                    content = $('content', this).first().html();
-                    show = $('default > value', this).first().html() === 'Yes';
-
                     locations.push({
-                        id: id,
-                        position: new google.maps.LatLng(lat, lng),
-                        title: title,
-                        content: content,
-                        show: show
-                    })
+                        id: $('id', this).first().html(),
+                        position: new google.maps.LatLng(
+                            Number($('latitude', this).first().html()),
+                            Number($('longitude', this).first().html())
+                        ),
+                        title: $('label', this).first().html(),
+                        content: $('content', this).first().html(),
+                        show: $('default > value', this).first().html() === 'Yes',
+                        url_image: $('image', this).first().html(),
+                        url_icon: $('icon', this).first().html(),
+                        address: $('address', this).first().html()
+                    });
                 });
 
                 // Determine the points that need to be shown at map startup
@@ -73,40 +76,101 @@
                 // use those coordinates
                 if (locationsShow.length) {
                     centerPos = locationsShow[0].position;
-                } else {
-                    centerPos = new google.maps.LatLng(centerLat, centerLng);
                 }
 
-                // Create map
-                map = new google.maps.Map(el, {
-                    center: centerPos,
-                    zoom: zoom,
-                    mapTypeId: mapTypeId
-                });
+                // Should we create the map now or upon a modal popup?
+                var $parent = $(el).closest(".modal");
 
-                // Create marker for each location and attach map to marker
-                locations.forEach(function (location) {
-                    location.map = map;
-                    location.marker = new google.maps.Marker({
-                        position: location.position,
-                        map: map,
-                        title: location.title
+                if ( $parent.length === 0 || $parent.is(":visible") ) {
+                    // We're not modal, or else the div is visible - create the map
+                    create_map();
+                } else {
+                    $parent.on('shown.bs.modal', function(){
+                        // Wait until the modal is shown, and then create the map
+                        create_map();
                     });
-                    location.infowindow = new google.maps.InfoWindow({
-                        content: location.content
-                    });
-                    location.marker.addListener('click', function(){
-                        location.infowindow.open(location.map, location.marker);
-                    });
-                });
-
-                // Show the info window for any default displayed marker
-                locationsShow.forEach(function (location) {
-                    location.infowindow.open(location.map, location.marker);
-                });
+                }
             }).fail(function (jqXHR, status, error) {
                 throw 'Failed to load map data. Status: ' + status;
             });
+
+            // Procedure to generate the Google map
+            function create_map () {
+                // Create map (if not already created)
+                if (typeof map === 'undefined') {
+                    map = new google.maps.Map(el, {
+                        center: centerPos,
+                        zoom: zoom,
+                        mapTypeId: mapTypeId
+                    });
+
+                    // Create marker for each location and attach map to marker
+                    locations.forEach(function (location) {
+                        var marker_options;
+
+                        // TODO - this may be a good place for the use of a template
+                        // If no additional content is specified, then use the title
+                        if (location.content === '') {
+                            location.content = location.title;
+                        }
+
+                        // Modify window content if there's an image specified
+                        if (location.url_image !== '') {
+                            location.content =
+                                "<img src='" + location.url_image + "'"
+                                + " alt='" + location.title + "'"
+                                + " style='max-width: 120px;'"
+                                + "/>"
+                                + "<div>"
+                                + location.content
+                                + "</div>";
+                        }
+
+                        // Modify window content if there's an address specified
+                        if (location.address !== '') {
+                            location.content += "<br/>"
+                                + location.address;
+                        }
+
+                        location.map = map;
+
+                        // Initialize the marker options to pass to constructor
+                        marker_options = {
+                            position: location.position,
+                            map: map,
+                            title: location.title
+                        };
+
+                        // If there's a icon specified then use that as a URL string
+                        if (location.url_icon !== '') {
+                            marker_options.icon = location.url_icon;
+                        }
+
+                        // Create the marker point on the map
+                        location.marker = new google.maps.Marker(marker_options);
+
+                        // Create the window that pops up on click and attach handler
+                        // to open the window upon click
+                        location.infowindow = new google.maps.InfoWindow({
+                            content: location.content
+                        });
+                        location.marker.addListener('click', function(){
+                            // Close all other windows
+                            locations.forEach(function (location) {
+                                location.infowindow.close();
+                            });
+
+                            // Open this window
+                            location.infowindow.open(location.map, location.marker);
+                        });
+                    });
+
+                    // Show the info window for any default displayed marker
+                    locationsShow.forEach(function (location) {
+                        location.infowindow.open(location.map, location.marker);
+                    });
+                }
+            }
         });
     });
 }));
