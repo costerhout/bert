bee12d7489f83886fde16ab9d1675ea8b24b6e6a
Colin Osterhout <colixx@gmail.com>
2016-05-31 19:33:12 -0800

    Now supports: * underscore.js mixins for easier function argument managment * better configuration of Handlebars Form Helpers * making headway towards testing configuration (not quite there yet) * runtime loading of templates from external resource

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,7 @@
 // License TBD - contact author. All rights reserved.
 define([
     // Standard items that export a variable
+    'module',
     'require',
     'jquery',
     'underscore',
@@ -21,14 +22,21 @@ define([
     'transitional/tabs',
     'transitional/modals',
 
+    // Handlebars helpers to be explicitly added
+    'hbs/handlebars',
+    'vendor/handlebars.form-helpers',
+
     // Add-ons to the above
     'jquery.xml2json'
-], function(require, $, _, TransitionalForms, TransitionalTabs, TransitionalModals) {
+], function(module, require, $, _, TransitionalForms, TransitionalTabs, TransitionalModals, Handlebars, HandlebarsFormHelpers) {
     'use strict';
 
     var modules = [];
 
     var initModules = function (options) {
+        // Reset the list of modules
+        modules.length = 0;
+
         $('div[data-module]').each(function () {
             // Save away the jQuery version of this element
             var el = this,
@@ -38,36 +46,91 @@ define([
             require(
                 ['modules/' + $(el).data('module')],
                 function (mod) {
-                    var module = new mod(
-                        // Build the options object by combining data- attributes,
-                        // module config options, and defaults specified in the DOM (if available)
-                        _.defaults(
-                            // Start with the data- attributes, omitting the module name and the defaults setting
-                            _.omit($(el).data(), ['module', 'defaults']),
-                            // Add in some default options
-                            {
-                                el: el,
-                                baseTemplateUrl: options.baseTemplateUrl,
-                                templateScheme: options.templateScheme
-                            },
-                            // Merge with defaults specified in the DOM (if available)
-                            {
-                                defaults: {
-                                    'json': $.parseJSON,
-                                    'xml': $.xml2json,
-                                    'noop': _.noop
-                                }[type]($defaults.html()) || {}
-                            }
-                        )
-                    );
-
                     // Push the newly created module on the stack
                     // We could do some sort of event here to alert subscribers in the future
-                    modules.push(module);
+                    modules.push(
+                        new mod(
+                            // Build the options object by combining data- attributes,
+                            // module config options, and defaults specified in the DOM (if available)
+                            _.defaults(
+                                // Start with the data- attributes, omitting the module name and the defaults setting
+                                _.omit($(el).data(), ['module', 'defaults']),
+                                // Add in some default options
+                                {
+                                    el: el,
+                                    baseTemplateUrl: module.config()['baseTemplateUrl'],
+                                    baseTemplateUrlInternal: module.config()['baseTemplateUrlInternal'],
+                                    templateScheme: options.templateScheme
+                                },
+                                // Merge with defaults specified in the DOM (if available)
+                                {
+                                    defaults: {
+                                        'json': $.parseJSON,
+                                        'xml': $.xml2json,
+                                        'noop': _.noop
+                                    }[type]($defaults.html()) || {}
+                                }
+                            )
+                        )
+                    );
                 }
             );
         });
     };
+
+    var assertDefined = function (val, key) {
+        if (_.isUndefined(val)) {
+            throw new Error("arguments[" + key + "] is undefined");
+        }
+    };
+
+    // Extend the _ object for easier argument management
+    // We only need to do this job once ever
+    var initUnderscore = _.once(function () {
+        _.mixin({
+            checkArgMandatory: function (options, argMandatory) {
+                _.map(arguments, assertDefined);
+
+                var argMissing =  _.difference(argMandatory, _.keys(options));
+
+                if (!(_.isEmpty(argMissing))) {
+                    throw new Error('Arguments missing: ' + argMissing.toString());
+                }
+
+                return options;
+            },
+
+            filterArg: function (options, argAllowed, fnAlert) {
+                _.map(Array.prototype.slice.call(arguments, 0, 2), assertDefined);
+
+                if (fnAlert === true) {
+                    fnAlert = _.bind(console.log, console);
+                } else if (!(_.isFunction(fnAlert))) {
+                     fnAlert = _.noop;
+                }
+
+                fnAlert('Extra arguments: ' +
+                    _.difference(_.keys(options), argAllowed).toString()
+                );
+
+                return _.pick(
+                    options,
+                    argAllowed
+                );
+            },
+
+            swapKeys: function (options, objKeyMap) {
+                _.map(arguments, assertDefined);
+
+                _.map(objKeyMap, function (val, key) {
+                    options[val] = options[key];
+                    delete options[key];
+                });
+
+                return options;
+            }
+        });
+    });
 
     var initTransitionalModules = function (options) {
         TransitionalForms.initialize();
@@ -77,6 +140,8 @@ define([
 
     var main = {
         initialize: function (options) {
+            HandlebarsFormHelpers.register(Handlebars);
+            initUnderscore();
             initModules(options);
             initTransitionalModules(options);
         }
