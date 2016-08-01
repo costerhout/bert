9e71037bce4aa25951c2892ec612886312399256
Colin Osterhout <colixx@gmail.com>
2016-08-01 15:00:09 -0800

    Added regional test module for Zopim. Unfortunately I can't get the before and after test functionality figured out, this will have to wait for another day...

--- test/bs2-tests.js
+++ test/bs2-tests.js
@@ -4,7 +4,7 @@
 * @Email:  ctosterhout@alaska.edu
 * @Project: BERT
 * @Last modified by:   ctosterhout
-* @Last modified time: 2016-07-26T16:25:54-08:00
+* @Last modified time: 2016-07-27T12:23:25-08:00
 * @License: Released under MIT License. Copyright 2016 University of Alaska Southeast.  For more details, see https://opensource.org/licenses/MIT
 */
 
@@ -17,13 +17,13 @@ require([
     'main',
     'helpers/bs2',
     '/tests/soundings-feed/test.js',
-    '/tests/zopim/test.js',
+    '/tests/zopim/test-regional.js',
     '/tests/mapdisplay/test.js'
-], function ($, _, mocha, chai, app, HandlebarsBootstrap, TestSoundingsFeed, TestZopim, TestMapdisplay) {
+], function ($, _, mocha, chai, app, HandlebarsBootstrap, TestSoundingsFeed, TestZopimRegional, TestMapdisplay) {
     var aTests = [
-        TestSoundingsFeed,
-        TestZopim,
-        TestMapdisplay
+        // TestSoundingsFeed,
+        TestZopimRegional,
+        // TestMapdisplay
     ];
 
     HandlebarsBootstrap.register();
@@ -53,8 +53,6 @@ require([
         _.each(aTests, function (Test) {
             var test = new Test(appinit, $testbed);
         });
-
-        // test = new TestSoundingsFeed(appinit, $testbed);
 
         // Run all the registered tests
         mocha.run();
--- test/tests/zopim/test-regional.js
+++ test/tests/zopim/test-regional.js
@@ -4,7 +4,7 @@
 * @Email:  ctosterhout@alaska.edu
 * @Project: BERT
 * @Last modified by:   ctosterhout
-* @Last modified time: 2016-07-25T13:22:22-08:00
+* @Last modified time: 2016-07-27T12:15:00-08:00
 * @License: Released under MIT License. Copyright 2016 University of Alaska Southeast.  For more details, see https://opensource.org/licenses/MIT
 */
 
@@ -12,7 +12,7 @@ define([
     'jquery',
     'underscore',
     '/tests/base.js',
-    'text!/tests/zopim/module.xml',
+    'text!/tests/zopim/regional.txt',
     'mocha',
     'chai',
 ], function ($, _, BaseTest, htmlModule, mocha, chai) {
@@ -20,41 +20,41 @@ define([
 
     var Test = BaseTest.extend({
         initialize: function (appinit, $testbed) {
-            var expect = chai.expect, that = this;
+            var expect = chai.expect,
+                that = this,
+                runTest = function(htmlModule) {
+                    var isLoaded = $.Deferred();
 
-            describe('Zopim', function() {
-                var isLoaded = $.Deferred();
-                // Set a really long timeout for Zopim
-                this.timeout(10000);
-
-                // Prior to test run: load up the module into the document
-                before(function () {
-                    // Define the module HTML code
-                    $testbed.empty();
+                    // Initialize the testbed with the module definition
                     $testbed.append(htmlModule);
 
                     // Initialize the application, letting it know that in the event of success to resolve our deferred object so our tests can start
                     appinit({
                         success: isLoaded.resolve
                     });
-                });
 
-                // Set the list of tests we need to run. Each one consists of a
-                // description string (desc) and a function (fn) which contains a
-                // simple set of chai statements
-                //
-                // Wait for loading to complete before running
-                that.queueTests([
-                    {
-                        desc: 'should have two Zopim iframe elements',
-                        fn: function () {
-                            expect($('.zopim iframe').length).to.eql(2);
+                    // Set the list of tests we need to run. Each one consists of a
+                    // description string (desc) and a function (fn) which contains a
+                    // simple set of chai statements
+                    //
+                    // Wait for loading to complete before running
+                    that.queueTests([
+                        {
+                            desc: 'should have two Zopim iframe elements',
+                            fn: function () {
+                                expect($('.zopim iframe').length).to.eql(2);
+                            }
                         }
-                    }
-                ], isLoaded.promise());
+                    ], isLoaded.promise());
+                };
 
-                // Cleanup after test run
-                after(function () {
+            describe('Zopim', function () {
+                this.timeout(10000);
+
+                before(function () {
+                    $testbed.empty();
+                });
+                after(function() {
                     // Clear out all the iframes
                     $('.zopim iframe').each(function (i, el) {
                         var $parent = $(el).parent();
@@ -62,9 +62,13 @@ define([
                         $parent.remove();
                     });
 
-                    // As a courtesy, clean out the div we loaded up earlier
+                    // Remove the external Zopim module from the global space
+                    delete window.$zopim;
+
                     $testbed.empty();
-                });
+                })
+
+                describe('Regional', _.partial(runTest, htmlModule));
             });
         }
     });
