105084f558056881977b04794f1eeb894a5e4734
Colin Osterhout <colixx@gmail.com>
2016-08-09 14:52:09 -0800

    Added "DataTables" plugin support to add filtering / searching capabilities to tables.

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,7 @@
 * @Email:  ctosterhout@alaska.edu
 * @Project: BERT
 * @Last modified by:   ctosterhout
-* @Last modified time: 2016-07-27T11:58:56-08:00
+* @Last modified time: 2016-08-08T14:17:39-08:00
 * @License: Released under MIT License. Copyright 2016 University of Alaska Southeast.  For more details, see https://opensource.org/licenses/MIT
 */
 
@@ -55,6 +55,11 @@ requirejs.config({
                 autoPlayOnLoad: 'true',
                 showThumbsOnLoad: 'false'
             }
+        },
+        'transitional/datatables': {
+            paging: false,
+            ordering: false,
+            searching: false
         }
     },
     shim: {
@@ -86,6 +91,7 @@ requirejs.config({
         'jquery.xml2json': 'vendor/jquery.xml2json',
         'handlebars': 'vendor/handlebars',
         'handlebars.runtime': 'vendor/handlebars.runtime',
+        'datatables': 'vendor/datatables',
         'modernizr': 'lib/modernizr',
 
         // --------------------------------------------------------
---
+++ src/js/transitional/datatables.js
@@ -0,0 +1,61 @@
+/**
+* @Author: Colin Osterhout <ctosterhout>
+* @Date:   2016-05-20T08:17:09-08:00
+* @Email:  ctosterhout@alaska.edu
+* @Project: BERT
+* @Last modified by:   ctosterhout
+* @Last modified time: 2016-08-08T14:21:18-08:00
+* @License: Released under MIT License. Copyright 2016 University of Alaska Southeast.  For more details, see https://opensource.org/licenses/MIT
+*/
+
+
+define([
+    'module',
+    'jquery',
+    'underscore',
+    'datatables'
+], function (module, $, _) {
+    'use strict';
+
+    // Instead of a constructor function we are returning a singleton module
+    return {
+        initialize: function (options) {
+            var initFilterTables = function () {
+                    // 'table-filtered' is the preferred class for tables which should be filtered
+                    $('.table-filtered')
+                    // There are some tables out there with this class as well
+                        .add('.data-table')
+                        .DataTable({
+                            searching: true
+                        });
+                },
+                initSortTables = function () {
+                    $("table[class*='table-autosort'").DataTable({
+                        ordering: true,
+                        searching: true
+                    });
+                };
+
+            // Move the modals around to the end of the body
+            try {
+                /* Only run if the DOM has finished loading */
+                $(function () {
+                    // Initialize the DataTable function within jQuery
+                    $.extend(true, $.fn.dataTable.defaults, module.config());
+
+                    // Find and initialize tables on the page
+                    initFilterTables();
+                    initSortTables();
+
+                    if (_.isFunction(options.success)) {
+                        options.success();
+                    }
+                });
+            } catch (e) {
+                if (_.isFunction(options.fail)) {
+                    options.fail(e.message);
+                }
+            }
+        }
+    };
+});
