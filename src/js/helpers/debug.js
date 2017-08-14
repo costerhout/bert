511ba86ea3963d5232bcd554ab86ae662ebfc956
Colin Osterhout <colixx@gmail.com>
2017-08-14 14:48:44 -0800

    Removed dependency on Handlebars' version of JSON object.

--- src/js/helpers/debug.js
+++ src/js/helpers/debug.js
@@ -3,12 +3,12 @@
 * @Date:   2016-05-27T14:15:29-08:00
 * @Email:  ctosterhout@alaska.edu
 * @Project: BERT
-* @Last modified by:   ctosterhout
-* @Last modified time: 2016-06-23T17:52:51-08:00
+ * @Last modified by:   ctosterhout
+ * @Last modified time: 2017-08-06T18:04:22-08:00
 * @License: Released under MIT License. Copyright 2016 University of Alaska Southeast.  For more details, see https://opensource.org/licenses/MIT
 */
 
-define(['underscore', 'hbs/handlebars', 'hbs/json2'], function (_, Handlebars, JSON) {
+define(['underscore', 'hbs/handlebars'], function (_, Handlebars) {
     'use strict';
 
     // Guard against the case where hbs/json2 is stubbed out
